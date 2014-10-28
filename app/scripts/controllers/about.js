68d07a0852563c6a3a018a10f5ab6f63e2fe1ce6
Arjun Basu <arjunkbasu@gmail.com>
2014-10-28 14:37:41 +0530

    Fixed the angular app name . Renamed it to 'TicTacToe' from 'App'
    The node module grunt-contrib-compass was missing, fixed it

--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -7,7 +7,7 @@
  * # AboutCtrl
  * Controller of the App
  */
-angular.module('App')
+angular.module('TicTacToe')
   .controller('AboutCtrl', function ($scope) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,7 +7,7 @@
  * # MainCtrl
  * Controller of the App
  */
-angular.module('App')
+angular.module('TicTacToe')
   .controller('MainCtrl', function ($scope) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
